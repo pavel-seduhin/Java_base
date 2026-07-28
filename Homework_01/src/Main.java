b75f5351d8f3fb104f8953c410b2ea87555ef94b
Pavel <pavel.seduhin@list.ru>
2026-07-28 18:19:28 +0300

    Initial commit

---
+++ Homework_01/src/Main.java
@@ -0,0 +1,9 @@
+//TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
+// нажмите на значок <icon src="AllIcons.Actions.Execute"/> в поле.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Нажмите <shortcut actionId="ShowIntentionActions"/>, когда курсор находится на выделенном тексте
+        // чтобы увидеть, как OpenIDE предлагает это исправить.
+        System.out.printf("Hello and welcome!");
+    }
+}
